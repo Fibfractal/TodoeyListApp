b8a1a51429525e175f8fffae5f195bc5232c016e
Rob <robert.systemdeveloper@gmail.com>
2020-11-25 19:05:24 +0100

    Added a populateList function

---
+++ script.js
@@ -0,0 +1,22 @@
+let items = [];
+
+function populateList(){
+    let item = {
+        item: "Spring en runda",
+        isChecked: false
+    };
+    let item1 = {
+        item: "Städa",
+        isChecked: false
+    };
+    let item2 = {
+        item: "Byt däck på bilen",
+        isChecked: false
+    };
+
+    items.push(item);
+    items.push(item1);
+    items.push(item2);
+}
+
+populateList();
